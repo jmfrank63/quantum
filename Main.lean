f3a80ae23f532f4c29e61e96e7ea402bbea16ed9
Johannes Maria Frank <jmfrank63@gmail.com>
2024-04-04 14:06:54 +0700

    Add license

--- Main.lean
+++ Main.lean
@@ -1,5 +1,4 @@
 import «Quantum»
 
 def main : IO Unit :=
-  IO.println s!"Run lean Quantum/Basic.lean to see the proof."
-  IO.println s!"We simply show no quantumical model with a valid classic model."
+  IO.println "Run lean Quantum/Basic.lean to see the proof. \n We simply show no quantumical model with a valid classic model."
