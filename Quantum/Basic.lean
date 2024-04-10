446dd1f97f91093a445b53435faffa9bfbfa5328
Johannes Maria Frank <jmfrank63@gmail.com>
2024-04-10 20:31:29 +0700

    Cleanup and more comments

--- Quantum/Basic.lean
+++ Quantum/Basic.lean
@@ -1,4 +1,4 @@
--- Define the Behavior type and specific behaviors a and b
+-- Define the Behavior type and specific behaviors
 inductive Behavior
 | classic : Behavior
 | quantum : Behavior
@@ -6,69 +6,73 @@ inductive Behavior
 | different : Behavior
 deriving Repr, DecidableEq
 
+-- Define the threshold for the model and the realms of the models
 def threshold : Nat := 100
+structure Scale :=
+(size : Nat)
 
 -- Define the Model structure
 structure Model :=
 (behavior : Bool → Nat → Option Behavior)
 (legal : Bool)
 
--- Define the Model structure
-structure DependencyModel :=
+-- Define the Dependent Model structure
+structure DependentModel :=
 (behavior : Nat → Option Behavior)
 (model: Model)
 
--- Define a new type that can be either a Model or a DependencyModel
-inductive ModelOrDependencyModel
-| Model (m : Model) : ModelOrDependencyModel
-| DependencyModel (d : DependencyModel) : ModelOrDependencyModel
-
-structure Scale :=
-(size : Nat)
+-- Define a new type that can be either a Model or a DependentModel
+inductive ModelOrDependentModel
+| Model (m : Model) : ModelOrDependentModel
+| DependentModel (d : DependentModel) : ModelOrDependentModel
 
 -- Define the Experiment structure
 structure Experiment :=
-(run : ModelOrDependencyModel → Nat → Option Behavior)
+(run : ModelOrDependentModel → Nat → Option Behavior)
 
--- Define Model A
+-- Define the Classic Model
 def ClassicModel (legal : Bool) : Model :=
-{ behavior := fun legal s => if legal then (if s > threshold then some Behavior.classic else some Behavior.different) else none,
+{ behavior := fun legal s => if legal then (if s > threshold then
+  some Behavior.classic else some Behavior.different) else none,
   legal := legal}
 
--- Define Model B based on Model A
-def QuantumModel (m: Model) (s : Nat): DependencyModel :=
+-- Define the Quantum Model as a dependent Model
+def QuantumModel (m: Model) (s : Nat): DependentModel :=
 { behavior := fun _ => if ¬ m.legal then m.behavior false s else
     if s < threshold then Behavior.quantum else Behavior.unknown,
   model := m }
 
 -- Define an experiment that uses a model's behavior function
 def experiment : Experiment :=
-{ run := λ modOrDepMod s => match modOrDepMod with
-  | ModelOrDependencyModel.Model m => match m.legal with
+{ run := fun modOrDepMod s => match modOrDepMod with
+  | ModelOrDependentModel.Model m => match m.legal with
     | false => none
     | true => (match m.behavior true s with
       | none => none
       | some _ => m.behavior true s)
-  | ModelOrDependencyModel.DependencyModel d => (match d.behavior s with
+  | ModelOrDependentModel.DependentModel d => (match d.behavior s with
       | none => none
       | some _ => d.behavior s)
 }
 
-def matchOptions {α : Type} [BEq α] (opt1 opt2 : Option α) : Bool :=
-  match opt1, opt2 with
-  | some x, some y => x == y
-  | none, none => true
-  | _, _ => false
+-- Define the experiments
+def classic := experiment.run (ModelOrDependentModel.Model
+                (ClassicModel true)) (threshold + 1)
+def classic_in_quantum := experiment.run (ModelOrDependentModel.Model
+                (ClassicModel true)) (threshold -1 )
+def quantum := experiment.run (ModelOrDependentModel.DependentModel
+                (QuantumModel (ClassicModel true) (threshold - 1))) (threshold -1)
+def quantum_in_classic := experiment.run (ModelOrDependentModel.DependentModel
+                (QuantumModel (ClassicModel true) (threshold + 1))) (threshold + 1)
 
-def classic := experiment.run (ModelOrDependencyModel.Model (ClassicModel true)) (threshold + 1)
-def classic_in_quantum := experiment.run (ModelOrDependencyModel.Model (ClassicModel true)) (threshold -1 )
-def quantum := experiment.run (ModelOrDependencyModel.DependencyModel (QuantumModel (ClassicModel true) (threshold - 1))) (threshold -1)
-def quantum_in_classic := experiment.run (ModelOrDependencyModel.DependencyModel (QuantumModel (ClassicModel true) (threshold + 1))) (threshold + 1)
+def illegal_classic := experiment.run (ModelOrDependentModel.Model
+                (ClassicModel false)) (threshold + 1)
+def illegal_quantum_in_quantum := experiment.run (ModelOrDependentModel.DependentModel
+                (QuantumModel (ClassicModel false) (threshold - 1))) (threshold - 1)
+def illegal_quantum_in_classic := experiment.run (ModelOrDependentModel.DependentModel
+                (QuantumModel (ClassicModel false) (threshold + 1))) (threshold + 1)
 
-def illegal_classic := experiment.run (ModelOrDependencyModel.Model (ClassicModel false)) (threshold + 1)
-def illegal_quantum_in_quantum := experiment.run (ModelOrDependencyModel.DependencyModel (QuantumModel (ClassicModel false) (threshold - 1))) (threshold - 1)
-def illegal_quantum_in_classic := experiment.run (ModelOrDependencyModel.DependencyModel (QuantumModel (ClassicModel false) (threshold + 1))) (threshold + 1)
-
+-- Run the experiments
 #eval classic
 #eval classic_in_quantum
 #eval quantum
@@ -80,8 +84,10 @@ def illegal_quantum_in_classic := experiment.run (ModelOrDependencyModel.Depende
 
 -- Theorem : If the classic model is illegal, then the quantum model is illegal
 theorem illegal_classic_implies_illegal_quantum (s : Nat) :
-  (ClassicModel false).legal = false → (QuantumModel (ClassicModel false) s).model.legal = false :=
-by intro h; exact h
+  (ClassicModel false).legal = false → (QuantumModel
+  (ClassicModel false) s).model.legal = false :=
+  by intro h; exact h
 
+-- Proof of the theorem
 #check illegal_classic_implies_illegal_quantum
 #eval "QED"
--- Quantum/tempCodeRunnerFile.lean
+++ Quantum/tempCodeRunnerFile.lean
@@ -1,1 +1,28 @@
-illegal_classic_implies_illegal_quantum
+def classic :=
+  experiment.run (ModelOrDependentModel.Model (ClassicModel true)) (threshold + 1)
+
+def classic_in_quantum :=
+  experiment.run (ModelOrDependentModel.Model (ClassicModel true)) (threshold -1)
+
+def quantum :=
+  experiment.run
+    (ModelOrDependentModel.DependentModel (QuantumModel (ClassicModel true) (threshold - 1)))
+    (threshold -1)
+
+def quantum_in_classic :=
+  experiment.run
+    (ModelOrDependentModel.DependentModel (QuantumModel (ClassicModel true) (threshold + 1)))
+    (threshold + 1)
+
+def illegal_classic :=
+  experiment.run (ModelOrDependentModel.Model (ClassicModel false)) (threshold + 1)
+
+def illegal_quantum_in_quantum :=
+  experiment.run
+    (ModelOrDependentModel.DependentModel (QuantumModel (ClassicModel false) (threshold - 1)))
+    (threshold - 1)
+
+def illegal_quantum_in_classic :=
+  experiment.run
+    (ModelOrDependentModel.DependentModel (QuantumModel (ClassicModel false) (threshold + 1)))
+    (threshold + 1)
